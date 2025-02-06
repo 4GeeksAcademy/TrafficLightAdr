14b2438c5e8d166d2939f29611fbf621067b1249
Aarón Adrián Huayna Silva <adrianhsilva403@gmail.com>
2025-02-06 19:48:44 +0000

    basic trafficLight

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,13 @@
 import React from "react";
+import TrafficLight from "./trafficLight";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+function App() {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	  <div>
+		<h1 style={{ color: 'white' }}>Semáforo en React</h1>
+		<TrafficLight />
+	  </div>
 	);
-};
-
-export default Home;
+  }
+  
+export default App;
---
+++ src/js/component/trafficLight.jsx
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from "react";
+
+const TrafficLight = () => {
+    
+    const colors = ['red', 'yellow', 'green'];
+    const [color, setColor] = useState(0); 
+ 
+    // Pasar al siguiente índice
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setColor(prevColor => (prevColor + 1) % colors.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+  
+    // Estilos
+    const trafficLightStyle = {
+      position: 'absolute',
+      top: '9.5%', 
+      left: '50%',
+      transform: 'translate(-65%, 0)', /* Ajusta el centro del semáforo */
+      width: '30%',
+      height: '40%',
+      // backgroundColor: 'black',
+      //borderRadius: '2vh',
+    };
+  
+    const estiloLuz = (luzColor) => ({
+      width: '8vh',
+      height: '8vh',
+      borderRadius: '50%',
+      backgroundColor: colors[color] === luzColor ? luzColor : "transparent",
+      margin: '3vh auto'
+    });
+  
+    return (
+      <div style={trafficLightStyle}>
+        <div style={estiloLuz('red')}></div>
+        <div style={estiloLuz('yellow')}></div>
+        <div style={estiloLuz('green')}></div>
+      </div>
+    );
+  };
+  
+  export default TrafficLight;
